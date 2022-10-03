4a4b69d6ae8e5fa8ada82fe8244ec9e51081a3b4
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2022-10-03 00:13:30 -0300

    update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,17 @@
-// Componentes / Propriedades
+import './styles/main.css';
 
-// img
+import logoImg from './assets/logo-nlw-esports.svg';
 
 function App() {
-  return <h1>Hello NLW</h1>
-  
+  return <div className='max-w-[1344] mx-auto flex flex-col items-center my-20'>
+    <img src={logoImg} alt="" />
+
+    <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'> duo</span> está aqui</h1>
+
+    <div className=''>
+
+    </div>
+  </div>;
 }
 
-export default App
+export default App;
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 45.57%)'
+      }
+    },
+  },
+  plugins: [],
+}
